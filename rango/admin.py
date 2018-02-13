35ab693878c6fc37c1e83c53663d1e84d6a2641b
Lucy Dunsmuir <lucymdunsmuir@gmail.com>
2018-02-13 13:12:48 +0000

    4
    reuploading out of venv for testing purposes

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from rango.models import Category, Page
+admin.site.register(Category)
+admin.site.register(Page)
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+class Meta:
+    verbose_name_plural = 'Categories'
+def __str__(self):
+    return self.name
