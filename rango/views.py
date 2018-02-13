adf246d8144e62cbc4d5404d9f6d373c7da8b49d
Lucy Dunsmuir <lucymdunsmuir@gmail.com>
2018-02-13 13:08:25 +0000

    2

--- rango/views.py
+++ rango/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 def index(request):
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     return render(request,'rango/index.html',context=context_dict)
-def about(request):return HttpResponse("Rango says here is the about page")
+def about(request):
+    my_dict ={'insert_me':"BOO!"}
+    return render(request,'rango/about.html',context=my_dict)
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^$', views.about, name='about'),
         url(r'^about', views.about, name='about'),
     url(r'^admin/', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
